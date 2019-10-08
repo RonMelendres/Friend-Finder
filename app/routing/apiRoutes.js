522e1b845137d7039733538d9f45fb8e3857d7ed
Ron Melendres <Ron.melendres25@gmail.com>
2019-10-07 18:44:42 -0700

    finished file exports and recording of user answers

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,20 @@
-// module.exports = function(app) {
+var friends = require("../data/friends.js");
 
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+    
+    app.post("/api/friends", function(req, res) {
+        var newFriends = req.body;
+        newFriends.name = newFriends.name.replace(/\s+/g, "").toLowerCase();
+        console.log(newFriends);
+        // console.log(req.body);
+        
+        friends.push(newFriends);
 
-// app.get("/api/friends", function(req, res) {
-//     return res.json(characters);
-// });
+        // res.json(newFriends);
+    
+    });
+}
 
-// app.post("/api/friends", function(req, res) {
-//     var newFriends = req.body;
-
-// });
-
-// }
--- server.js
+++ server.js
@@ -9,80 +9,83 @@ var PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-var friends = [
-    {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    }
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-];
+// var friends = [
+//     {
+//         name: "Ahmed",
+//         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//         scores: [
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1
+//         ]
+//     },
+//     {
+//         name: "Ahmed",
+//         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//         scores: [
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1
+//         ]
+//     },
+//     {
+//         name: "Ahmed",
+//         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//         scores: [
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1
+//         ]
+//     }
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
+// ];
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/home.html"));
+// });
 
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-});
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+// });
 
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-});
+// app.get("/api/friends", function(req, res) {
+//     return res.json(friends);
+// });
 
-app.post("/api/friends", function(req, res) {
-    var newFriends = req.body;
-    newFriends.name = newFriends.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newFriends);
-    friends.push(newFriends);
+// app.post("/api/friends", function(req, res) {
+//     var newFriends = req.body;
+//     newFriends.name = newFriends.name.replace(/\s+/g, "").toLowerCase();
+//     console.log(newFriends);
+//     friends.push(newFriends);
 
-    res.json(newFriends);
+//     res.json(newFriends);
 
-});
+// });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
